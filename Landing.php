1b0b282cbe42b738639ac6f78a7e50763596784e
takuya kido <takuyakido1993@gmail.com>
2021-01-18 06:51:25 +0900

    新規作成

---
+++ Landing.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8" />
+        <title>WinWin</title>
+        <meta name="description" content="5Gの出会いを友達と体感!!友達といる時にグループ同士で出会えたら楽しそうじゃないですか??福岡の夜を友達と楽しみたいならこのアプリしかない!!">
+        <!-- レスポンシブデザイン用のタグ -->
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <!-- アイコンの画像表示 -->
+        <link rel="icon" href="img/winwin.png" sizes="16x16" type="image/png" />
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="css/slick.css"/>
+        <link rel="stylesheet" href="css/style.css" />
+        <!-- Javascript -->
+        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script type="text/javascript" src="js/slick.min.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>
+       
+        <!-- adobeのフォント -->
+        <script>
+          (function(d) {
+            var config = {
+              kitId: 'her3jaq',
+              scriptTimeout: 3000,
+              async: true
+            },
+            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+          })(document);
+        </script>
+        <script>
+          (function(d) {
+            var config = {
+              kitId: 'her3jaq',
+              scriptTimeout: 3000,
+              async: true
+            },
+            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+          })(document);
+        </script>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700&display=swap" rel="stylesheet">
+        <!-- レスポンシブを読み込む -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    </head>
+
+    <body>
+        <!-- ファーストビューの実装 -->
+        <div class="lp_first_view">
+            <img class="lp_profile" src="img/profile.PNG" >
+            <img class="lp_winwin" src="img/winwin_top.PNG" >
+            <span class="lp_span">福岡初！地域限定</span>
+            <p><span class="lp_main">5Gの出会い</span><br>を友達と体感</p>
+            <a href="http://localhost/landing.php" class="lp_first_button">今すぐ簡単無料登録！</a>
+            <img class="first_apple" src="img/apple.png" >
+            <img class="first_google" src="img/google.png" >
+        </div>
+        <!-- WinWinとはの実装 -->
+        <div class="lp_what_is_winwin">
+                <div class="what_is_winwin">
+                    <img class="what_is_winwin_title_img" src="img/winwin_cloud.jpg">
+                    <div class="what_is_winwin_title">
+                        <p> <span class="what_is_winwin_span">Win Win </span>って何？</p>
+                    </div>
+                    <div class="what_is_winwin_description">
+                        <h1>その日のうちに友達同士でのマッチングを可能にした<br>地域限定のアプリです！</h1>
+                    </div>
+                </div>
+                <!-- こっからコンテンツ部分 -->
+                <div id="contents_slider" class="contents">
+                    <div class="winwin_content">
+                        <h2 class="content_title">この瞬間に出会いたい、、</h2>
+                        <img class="content_img" src="img/content1.JPG">
+                        <p class="content_text">WinWinでは<span class="content_span">「リアルタイム」</span><br>で近くのユーザーが表示されます！<br>その日のうちにマッチングできるアプリです！</p>
+                        <h1>早い!</h1>
+                    </div>
+                    <div class="winwin_content">
+                        <h2 class="content_title">友達といるけど、、</h2>
+                        <img class="content_img" src="img/content2.jpg">
+                        <p class="content_text">WinWinでは<span class="content_span">「友達同士」</span><br>でのマッチングを実現するため、全てのユーザーが２人以上です！(最大５人まで)</p>
+                        <h1>複数人!！</h1>
+                    </div>
+                    <div class="winwin_content">
+                        <h2 class="content_title">場所を気にしたくない、、</h2>
+                        <img class="content_img" src="img/content3.JPG">
+                        <p class="content_text">WinWinでは<span class="content_span">「遊びたい場所」</span><br>で人の目を気にせずに遊べます！<br>場所や時間に縛られずに楽しみましょう！</p>
+                        <h1>どこでも！!！</h1>
+                    </div>
+                </div>
+        </div>
+        <!-- 使い方説明の部分 -->
+        <div class="lp_how_to_use">
+            <!-- 使い方のタイトル部分 -->
+            <div class="how_to_use_title">
+                <img class="how_to_use_img" src="img/winwin_cloud.jpg">
+                <h2>How To Use?</h2>
+                <p>使い方はかんたん</p>
+            </div>
+            <!-- 使い方のタイトル部分 -->
+            <div class="how_to_use_contents">
+                <div class="how_to_content">
+                    <p class="how_to_No">01</p>
+                    <img class="how_to_content_img" src="img/HowTo1.png">
+                    <h3>まずは近くにいる<br>グループを検索します</h3>
+                    <p class="how_to_description">近くにいるグループを探します。<br>相手グループの条件から理想の<br>グループを探しましょう。</p>
+                </div>
+                <div class="how_to_content">
+                    <p class="how_to_No">02</p>
+                    <img class="how_to_content_img" src="img/HowTo2.png">
+                    <h3>「良いね！」を送って<br>相手グループにアピール</h3>
+                    <p class="how_to_description">気になったグループに「良いね！」を送って<br>マッチングをしましょう。</p>
+                </div>
+                <div class="how_to_content">
+                    <p class="how_to_No">03</p>
+                    <img class="how_to_content_img" src="img/HowTo3.png">
+                    <h3>マッチングが成立後<br>すぐ会える</h3>
+                    <p class="how_to_description">WinWinの最大の特徴は<br>マッチング後にメッセージで<br>場所を決めるだけで会えること！<br>出会うまでの負担を最大限に減らしています。</p>
+                </div>
+            </div>
+        </div>
+        <!-- フッターの部分 -->
+        <div class="lp_footer">
+            <div class="footer_main">
+                <div class="questions">
+                    <div class="text1">
+                        <h1>今友達といる？</h1>
+                    </div>
+                    <div class="text2">
+                        <h1>異性とワイワイしたい？</h1>
+                    </div>
+                    <div class="text3">
+                        <h1>場所を考えず</h1>
+                        <h1>はしゃぎたい？</h1>
+                    </div>
+                </div>
+                <div class="download">
+                    <div class="download_text">
+                        <p>叶えるなら、今すぐ</p>
+                        <h1>WinWin使うしかなかろーもん！</h1>
+                    </div>
+                    <div class="download_img">
+                        <img class="footer_apple" src="img/apple.png" >
+                        <img class="footer_google" src="img/google.png" >
+                    </div>
+                </div>
+            </div>
+        <div class="footer_sub">
+            <img class="footer_winwin" src="img/winwin_footer.PNG" >
+            <p>Copyright © 2020 YOLO Inc. All Rights Reserved.</p>
+            <p>運営会社　｜　個人情報保護方針</p>
+        </div>
+        </div>
+    </body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,14 @@
+// $(function () {
+//   $(window).on("load resize", function () {
+//     var winW = $(window).width();
+//     var devW = 480;
+//     const slider = $("#contents_slider");
+//     if (winW <= devW) {
+//       console.log(slider);
+//       slider.addClass("contents_slider");
+//       $(".contents_slider").slick();
+//     } else {
+//       slider.removeClass("contents_slider");
+//     }
+//   });
+// });
